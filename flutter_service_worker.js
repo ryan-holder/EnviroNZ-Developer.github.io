b2a6542b35d8cb762462d8cfdc1833e16097f383
Matt List <mattylist@gmail.com>
2020-12-16 12:07:04 +1300

    #7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {
 "index.html": "861cdf8e6bb6724720889264264e7728",
 "/": "861cdf8e6bb6724720889264264e7728",
 "leaf.png": "4ffacdcca2481573c09930dc6be5cea6",
-"main.dart.js": "8bc1d2aff4274cff2eb4c6e585e8dfdb",
+"main.dart.js": "a7757ed79dd8fb49751236da9f32ec41",
 "manifest.json": "adc25949bf0f463fe04548cacc80513c",
 "version.json": "2bac15195b42a0c3ccd64dd563fe9918"
 };
